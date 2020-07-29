5581f8d5b754d03cc72c53ceb4f10db4e9127c5f
yahiya_mansuri <yahiyamansuri14@gmail.com>
2020-07-29 23:28:58 +0530

    Create TSORT.cpp
    We used an hashtable with linear probing for the solution in given time. Quick sort too will not work here.

---
+++ TSORT.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+ using namespace std;
+ int arr[1000000]={0};
+ int main(){
+     int i,n,a;
+     cin>>n;
+     while(n--){
+         cin>>a;
+         arr[a]++;
+     }
+     a=0;
+     while(a<1000000){
+         while(arr[a]){
+             cout<<a<<endl;
+             arr[a]--;
+         }
+        a++;
+     }
+     return 0;
+ }
